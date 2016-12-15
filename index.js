01ad7cb42482a2d4aa8eb1d85d377aa97faf631e
Kobi Shasha <kobi.shasha@gmail.com>
2016-12-15 11:18:54 +0200

    Echo messages switch
    add switch

--- index.js
+++ index.js
@@ -16,8 +16,58 @@ app.get('/', function (req, res) {
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+        sendMessage(event.sender.id, { text: "a1" });
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Invalid verify token');
     }
 });
+
+// handler receiving messages
+app.post('/webhook', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text)
+        {
+            switch (event.message.text) {
+                case "hello": {
+                    sendMessage(event.sender.id, { text: "hello" });
+                    break;
+                }
+                case "monyx": {
+                    sendMessage(event.sender.id, { text: "best app ever"});
+                    break;         
+                }
+                case "phone": {
+                    sendMessage(event.sender.id, { text: "054-123456789" });
+                }
+                
+            }
+           // sendMessage(event.sender.id, { text: "Echo: " + event.message.text });
+        }
+        else {
+            sendMessage(event.sender.id, { text: "a2" });
+        }
+    }
+    res.sendStatus(200);
+});
+
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: {
+            recipient: { id: recipientId },
+            message: message,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
